d4ec452251bc68a086d7ec14be950ddc510f8dbd
Ivan Matkov <ivan.matkov@jetbrains.com>
2023-10-09 14:37:16 +0200

    Generate initial project

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Non_Common_API_Issue"
+include(":androidApp")
+include(":shared")
